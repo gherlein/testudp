13ccbb2ed3aa6201e89ed3867777dadb85a4936e
Greg Herlein <gherlein@herlein.com>
2021-12-05 16:32:20 -0800

    working

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,19 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
+import dgram = require('dgram');
+let count = 0;
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
+const server = dgram.createSocket('udp4');
+server.on('error', (err) => {
+    console.log(`server error:\n${err.stack}`);
+    server.close();
+});
+server.on('message', (msg, senderInfo) => {
+    console.log('udp (' + senderInfo.address + ') [' + count + ']: ' + msg);
+    count += 1;
+});
+server.on('listening', () => {
+    const address = server.address();
+    console.log(`server listening on ${address.address}:${address.port}`);
+});
+server.bind(5500);
 
-// Below are examples of using ESLint errors suppression
-// Here it is suppressing a missing return type definition for the greeter function.
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function greeter(name: string) {
-  return await delayedHello(name, Delays.Long);
-}
+// https://stackoverflow.com/questions/11290553/node-js-udp-data-lost-at-high-package-rates
